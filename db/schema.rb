4bdf00063678a84c6d31508bb9edaf6c8c22ae65
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2023-11-07 21:33:06 +0200

    before T5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_07_173545) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_07_185723) do
+  create_table "beers", force: :cascade do |t|
+    t.string "name"
+    t.string "style"
+    t.integer "brewery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "breweries", force: :cascade do |t|
     t.string "name"
     t.integer "year"
