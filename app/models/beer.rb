255cb41b17fd602884ce171a0095e3060bba2680
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2023-11-11 16:54:02 +0200

    W2 T4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,16 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        if self.ratings.empty?
+            return nil
+        end
+        c = self.ratings.count.to_f
+        ss = 0.0
+        self.ratings.each do |rat|
+            ss = ss + rat.score
+        end
+        ss / c
+    end
 end
