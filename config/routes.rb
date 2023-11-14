57136b76f65eb5abbe6c95ad0dd8f43645f29268
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2023-11-14 19:27:10 +0200

    W3 pre 4

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
 
   get 'kaikki_bisset', to: 'beers#index'
   get 'signup', to: 'users#new'
-
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
+  
   # get 'ratings', to: 'ratings#index'
   # get 'ratings/new', to:'ratings#new'
   # post 'ratings', to: 'ratings#create'
