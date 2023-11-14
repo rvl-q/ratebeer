17d0ab4964346ff4f9ac2280fc800943ec97774f
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2023-11-14 21:17:27 +0200

    W3 pre 5

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,6 @@
 class Rating < ApplicationRecord
   belongs_to :beer
+  belongs_to :user   # rating kuuluu myös käyttäjään
 
   def to_s
     "#{beer.name} #{score}"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  has_many :ratings   # käyttäjällä on monta ratingia
 end
