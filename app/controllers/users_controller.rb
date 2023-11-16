cfeca9976b9e08c3d8d8112bbe55fd3c698d4e89
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2023-11-16 23:50:12 +0200

    W3 pre 16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,14 +36,12 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    return unless @user == current_user
-
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
+      if user_params[:username].nil? and @user == current_user and @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render action: 'edit' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
