235f5ab4b524de6515b277a3205ed75b27cb10fe
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2023-11-16 14:49:20 +0200

    W3 T12

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,17 @@ class User < ApplicationRecord
 
   validates :username,  uniqueness: true,
                         length: { in: 3..30 }
+  validates :password, length: { minimum: 4 }
+  validate :validate_pwd
 
   has_many :ratings
   has_many :beers, through: :ratings
   has_many :memberships
   has_many :beer_clubs, through: :memberships
+
+  def validate_pwd
+    return unless (password.count("0-9") < 1) || (password.count("A-Z") < 1)
+
+    errors.add(:password, "must contain at least one number and one Capital letter")
+  end
 end
