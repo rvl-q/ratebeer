a0c940a0b8ab564950d35b34387e752f4b2d8607
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2023-11-17 17:04:26 +0200

    W4 T1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,20 @@ RSpec.describe User, type: :model do
     expect(User.count).to eq(0)
   end
 
+  it "is not saved with a too short password" do
+    user = User.create username: "Pekka", password: "Se1", password_confirmation: "Se1"
+
+    expect(user).not_to be_valid
+    expect(User.count).to eq(0)
+  end
+
+  it "is not saved with an invalid password" do
+    user = User.create username: "Pekka", password: "secr", password_confirmation: "secr"
+
+    expect(user).not_to be_valid
+    expect(User.count).to eq(0)
+  end
+
   describe "with a proper password" do
     let(:user){ User.create username: "Pekka", password: "Secret1", password_confirmation: "Secret1" }
     let(:test_brewery) { Brewery.new name: "test", year: 2000 }
