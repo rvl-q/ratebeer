860441ef8ef32ad1a0de52314598a77657d6174b
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2023-11-21 23:21:05 +0200

    test: with one rating should match that rating

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -109,6 +109,13 @@ RSpec.describe User, type: :model do
     it "without ratings does not have a favorite brewey" do
       expect(user.favorite_brewery).to eq(nil)
     end
+
+    it "it corresponds to the only rated if only one rating" do
+      beer = FactoryBot.create(:beer)
+      rating = FactoryBot.create(:rating, score: 20, beer: beer, user: user)
+
+      expect(user.favorite_brewery).to eq(beer.brewery)
+    end
   end
 end
 
