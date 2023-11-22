a6450ba71892199409b3b809028441f9320133fb
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2023-11-23 01:36:04 +0200

    W4 T9

--- spec/features/ratings_page_spec.rb
+++ spec/features/ratings_page_spec.rb
@@ -38,27 +38,44 @@ describe "Rating" do
     expect(page).to have_content 'Number of ratings: 4'
   end
 
-  it "user page should show own and only own ratings" do
-    user2 = FactoryBot.create :user, username: 'Patka'
-    FactoryBot.create :rating, score: '23', user:user, beer:beer1
-    FactoryBot.create :rating, score: '24', user:user, beer:beer2
-    FactoryBot.create :rating, score: '25', user:user, beer:beer1
-    FactoryBot.create :rating, score: '26', user:user, beer:beer2
-    FactoryBot.create :rating, score: '35', user:user2, beer:beer1
-    FactoryBot.create :rating, score: '36', user:user2, beer:beer2
-    visit user_path(user)
-    expect(page).to have_content 'Has made 4 ratings'
-  end
+  describe "on user page" do
+    it "should show own and only own ratings" do
+      user2 = FactoryBot.create :user, username: 'Patka'
+      FactoryBot.create :rating, score: '23', user:user, beer:beer1
+      FactoryBot.create :rating, score: '24', user:user, beer:beer2
+      FactoryBot.create :rating, score: '25', user:user, beer:beer1
+      FactoryBot.create :rating, score: '26', user:user, beer:beer2
+      FactoryBot.create :rating, score: '35', user:user2, beer:beer1
+      FactoryBot.create :rating, score: '36', user:user2, beer:beer2
+      visit user_path(user)
+      expect(page).to have_content 'Has made 4 ratings'
+    end
 
-  it "deleting rating through user page should remove the rating from the db" do
-    FactoryBot.create :rating, score: '35', user:user, beer:beer1
-    FactoryBot.create :rating, score: '36', user:user, beer:beer2
-    visit user_path(user)
-    # Delete rating for beer2
-    find(:xpath, "(//a[text()='Delete'])[2]").click
-    expect(page).to have_content 'Has made 1 rating'
-    expect(Rating.count).to eq(1)
-    expect(beer1.ratings.count).to eq(1)
-    expect(beer2.ratings.count).to eq(0)
+    it "deleting rating through here should remove the rating from the db" do
+      FactoryBot.create :rating, score: '35', user:user, beer:beer1
+      FactoryBot.create :rating, score: '36', user:user, beer:beer2
+      visit user_path(user)
+      # Delete rating for beer2
+      find(:xpath, "(//a[text()='Delete'])[2]").click
+      expect(page).to have_content 'Has made 1 rating'
+      expect(Rating.count).to eq(1)
+      expect(beer1.ratings.count).to eq(1)
+      expect(beer2.ratings.count).to eq(0)
+    end
+
+    it "without ratings shold say 0 ratings and nothing about favourite brewery or style" do
+      visit user_path(user)
+      expect(page).to have_content 'Has made 0 ratings'
+      expect(page).to have_no_content 'Favourite style:'
+      expect(page).to have_no_content 'Favourite brewery:'
+    end
+
+    it "with ratings shold show favourite brewery and style" do
+      FactoryBot.create :rating, score: '35', user:user, beer:beer1
+      FactoryBot.create :rating, score: '36', user:user, beer:beer2
+      visit user_path(user)
+      expect(page).to have_content 'Favourite style: Lager'
+      expect(page).to have_content 'Favourite brewery: Koff'
+    end
   end
 end
