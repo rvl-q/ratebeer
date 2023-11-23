209243bc7fb66e4d33d36b1d08008533eb7458ad
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2023-11-23 23:26:07 +0200

    places_spec.

---
+++ spec/features/places_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Places" do
+  it "if one is returned by the API, it is shown at the page" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [ Place.new( name: "Oljenkorsi", id: 1 ) ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+  end
+end
