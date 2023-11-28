39ea3bd0bccd4660caa17894a35a8664c59f9b30
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2023-11-28 22:32:16 +0200

    W5 T12

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -5,28 +5,42 @@ class StylesController < ApplicationController
 
   def show
     @style = Style.find(params[:id])
+    @beers = Beer.where style_id: @style.id
   end
 
-  # def new
-  #   @rating = Rating.new
-  #   @beers = Beer.all
-  # end
+  def new
+    @style = Style.new
+  end
 
-  # def create
-  #   @rating = Rating.new params.require(:rating).permit(:score, :beer_id)
-  #   @rating.user = current_user
+  def create
+    @style = Style.new params.require(:style).permit(:name, :description)
 
-  #   if @rating.save
-  #     redirect_to user_path current_user
-  #   else
-  #     @beers = Beer.all
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
+    if current_user && @style.save
+      redirect_to @style, notice: 'Style was successfully created.'
+    else
+      # @styles = Style.all
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-  # def destroy
-  #   rating = Rating.find params[:id]
-  #   rating.delete if current_user == rating.user
-  #   redirect_to user_path(current_user)
-  # end
+  def update
+    @style = Style.find(params[:id])
+
+    if current_user && @style.update(params.require(:style).permit(:name, :description))
+      redirect_to @style, notice: 'Style was successfully updated.'
+    else
+      # @styles = Style.all
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @style = Style.find(params[:id])
+  end
+
+  def destroy
+    style = Style.find params[:id]
+    style.delete if current_user
+    redirect_to styles_path, notice: 'Style was successfully deleted.'
+  end
 end
--- app/models/style.rb
+++ app/models/style.rb
@@ -1,3 +1,5 @@
 class Style < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+
   has_many :beers, dependent: :destroy
 end
