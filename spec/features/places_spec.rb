d8fd3f708c0b81db78f197e2bd36bc836f5f5faa
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2023-11-30 01:52:49 +0200

    W5 T16

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 describe "Places" do
   it "if one is returned by the API, it is shown at the page" do
     allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
-      [ Place.new( name: "Oljenkorsi", id: 1 ) ]
+      [ Place.new( name: "Oljenkorsi", id: 1, city: "Kumpula" ) ]
+    )
+    allow(WeatherApi).to receive(:weather_in).with("Kumpula").and_return(
+      Weather.new( temperature: -1, wind_speed: 20, wind_dir: "S", location: "Kumpula", weather_icons: [] )
     )
 
     visit places_path
@@ -26,12 +29,15 @@ describe "Places" do
   end
 
   it "if many returned by the API, all are shown on the page" do
-    places = [Place.new( name: "Bar1", id: 1 ),
+    places = [Place.new( name: "Bar1", id: 1, city: "Helsinki" ),
               Place.new( name: "Bar2", id: 2 ),
               Place.new( name: rand_bar_name, id: 3 )]
 
     allow(BeermappingApi).to receive(:places_in).with("helsinki").and_return(
       places
+    )
+    allow(WeatherApi).to receive(:weather_in).with("Helsinki").and_return(
+      Weather.new( temperature: -1, wind_speed: 20, wind_dir: "S", location: "Helsinki", weather_icons: [] )
     )
 
     visit places_path
