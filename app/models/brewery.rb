74a430aa2a63ff479ff4ef8d679d8f6a3c3f5f0a
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2023-12-05 13:33:37 +0200

    W6 pre 6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,28 +1,14 @@
 class Brewery < ApplicationRecord
-  include RatingAverage
-
-  validates :name, presence: true
-  validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 1040 }
-  validate :year_cannot_be_in_the_future
-
-  def year_cannot_be_in_the_future
-    t = Time.now.year
-    return unless year.present? && year > t
-
-    errors.add(:year, "cannot be in the future (>#{t})")
-  end
-
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
-  def print_report
-    puts name
-    puts "established at year #{year}"
-    puts "number of beers #{beers.count}"
-  end
+  validates :name, presence: true
+  validates :year, numericality: { only_integer: true,
+                                   greater_than: 1039,
+                                   less_than_or_equal_to: ->(_) { Time.now.year } }
 
-  def restart
-    self.year = 2022
-    puts "changed year to #{year}"
-  end
+  scope :active, -> { where active: true }
+  scope :retired, -> { where active: [nil, false] }
+
+  include RatingAverage
 end
