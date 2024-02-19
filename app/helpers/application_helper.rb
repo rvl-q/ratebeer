885b5a0e005a29ffb13e25445b921ce1d92e28e5
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2024-02-19 20:23:01 +0200

    app helper pre T9

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def edit_and_destroy_buttons(item)
+    unless current_user.nil?
+      edit = link_to('Edit', url_for([:edit, item]), class: "btn btn-primary")
+      # del = link_to('Destroy', item, method: :delete, # this may work in hotwire >7.3?
+      #                                form: { data: { turbo_confirm: "Are you sure ?" } },
+      del = button_to('Destroy', item, method: :delete, # workaround
+                                     form: { data: { turbo_confirm: "Are you sure ?" } },
+                                     class: "btn btn-danger")
+      raw("#{edit} #{del}")
+    end
+  end
 end
