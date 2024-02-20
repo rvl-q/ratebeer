3454a5bb793fcb98bba90531ddf5490869337107
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2024-02-20 03:50:24 +0200

    better password freedom

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :username,  uniqueness: true,
                         length: { in: 3..30 }
   validates :password, length: { minimum: 4 },
-                       format: { with: /\A[A-Z].*\d|\d.*[A-Z]\z/, message: "must include one upper case letter and number" }
+                       format: { with: /\A.*[A-Z].*\d.*|.*\d.*[A-Z].*\z/, message: "must include one upper case letter and number" }
 
   has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
