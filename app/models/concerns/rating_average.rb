501da12fe39985499e09589199927afbf33dbce4
Ragnar Lindqvist <ragnar.lindqvist@aalto.fi>
2024-02-24 04:23:44 +0200

    rubocop pre T9

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -14,8 +14,9 @@ module RatingAverage
   def average_rating
     # tehdään laskelmat muistiin haettujen olueen liittyvien ratings-olioiden avulla
     rating_count = ratings.size
-    
+
     return 0 if rating_count == 0
-    ratings.map{ |r| r.score }.sum / rating_count
+
+    ratings.map(&:score).sum / rating_count
   end
 end
